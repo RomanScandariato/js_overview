1a5caaf299cea1c677a573b0a90b68d854344396
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-09-18 13:03:36 -0400

    finished array basics

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Arrays are not primitive value types
+// Thet are reference types
+
+const fruits = ['apple', 'orange', 'strawberry', 'grape'];
+const names = ['Roman', 'Michael', 'Nina', 'Adonis'];
+const data = ['JD', 'Tadlock', 44, '555 coding dr', '777-777-7777', ['pickleball', 'fishing']];
+
+fruits[2] = 'strawberry';
+
+// Increase te data age number by one - console.log the original value and the new value after you increase it
+
+console.log(data[2]);
+
+data[2] ++ 
+
+console.log(data[2]);

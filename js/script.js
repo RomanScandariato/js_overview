f61eebc5228c78df76cb5ab52201f956c3b5ef96
Roman Scandariato <romanscandariato@Romans-MacBook-Pro.local>
2024-09-18 11:55:38 -0400

    finished conditional overview

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
-const firstName = 'Roman';
-let age = 5;
+const firstName = 'JD';
+let age = 16;
 
-// age = age + 5;
-//age += 5;
+if (age >20 && age < 50) {
+    console.log('you are young')
+} else if (age >= 50) {
+    console.log('you are old bruv')
+} else if (age <20 && age > 12) {
+    console.log('you are a teenager')
+} else {
+    console.log('invalid age')
+}
 
-// age = age -5
-//age -= 5;
 
-//age *= 5;
+// If age is greater than 20 and less than 50, console log 'you are young'
 
-// age = age + 1;
-age++;
+// If age is greater than or equal to 50, console log 'you are old bruv'
 
-console.log(age);
+// If age is less than 20 and greater than 12, console log 'you are a teenager'
+
